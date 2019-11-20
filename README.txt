f7bc4394e7b017a9561f804d62f3b30dc0e6c3fd
a-delmas <aurore.delmas@hotmail.fr>
2019-11-20 16:55:18 +0100

    End day.

---
+++ README.txt
@@ -0,0 +1,6 @@
+Points forts : Aucun
+
+Lacunes : compréhension des notions à utiliser mais oublis de l'utilisation de certains "procédés" (SASS, Node_Modules, ...), ...
+
+Realisation : déjà que j'avais des difficultés à réaliser le projet de base, j'ai préférée me concentrer dessus afin d'avoir un résulat correct.
+Afin de mieux voir ce que je faisais, j'ai utilisée une copie "modifiée" de la BDD.
--- script.js
+++ script.js
@@ -2,24 +2,43 @@ $(document).ready(function () {
 
     for (let i = 0; i < pictures.length; i++) {
         var card = $('<div>')
-        card.attr("class", "col-3")
+        card.attr("class", "row")
+        card.attr("class", "col-12")
         $("#votes").append(card)
 
         var url = $("<img src=" + pictures[i].url + "></img>")
+        url.css("width", "100%", "height", "100%")
 
         // var id = $("<p>" + pictures[i].id + "</p>")
-       
+
         var title = $("<p>" + pictures[i].title + "</p>")
         title.css("background-color", "grey")
-        title.attr("class", "p-13")
- 
+        title.attr("class", "p-0 mb-0")
+
         var subtile = $("<p>" + pictures[i].subtile + "</p>")
         subtile.css("background-color", "grey")
-        
-        var votes = $("<p>" + '<i class="fas fa-poo mr-1 btn-danger fa-4 rounded-circle"></i>' + pictures[i].votes + '<i class="fas fa-heart ml-1 btn-success fa-4 rounded-circle"></i>' + "</p>")
+        subtile.attr("class", "p-0 mb-0")
+
+        // var btn1 = $('<i class="fas fa-poo"></i>');
+        // var btn2 = $('<i class="fas fa-heart"></i>');
+        // var text = $("pictures[i].votes");
+        // votes.append(btn1);
+        // votes.append(btn2);
+        // votes.append(text);
+        // var cpt = 0;
+        // btn1.click(function () {
+        //     cpt = cpt - 1;
+        //     text.val(cpt)
+        // });
+        // btn2.click(function () {
+        //     cpt = cpt + 1;
+        //     text.val(cpt)
+        // });
+        var votes = $("<p>" + '<i class="fas fa-poo mr-1 btn-danger fa-2x rounded-circle"></i>' + pictures[i].votes + " vote(s) " + '<i class="fas fa-heart ml-1 btn-success fa-2x rounded-circle"></i>' + "</p>")
         votes.css("background-color", "grey")
+        votes.attr("class", "p-0 mb-5")
 
         card.append(url, title, subtile, votes)
     }
 })
-
+// });
